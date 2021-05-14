5238e7aaa4c5c2e1c85c3ba4f21a267ad9ea527e
Elena-Weber <crochetmiracles@gmail.com>
2021-05-14 17:42:07 -0400

    add flash errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -46,6 +46,7 @@ class SessionsController < ApplicationController
 
     def destroy
         session.clear
+        flash[:notice] = "You've successfully logged out."
         redirect_to login_path
     end
 
